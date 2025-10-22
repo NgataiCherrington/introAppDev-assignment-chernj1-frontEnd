fb6a092f080fe0d8415633424679cb9674757b9b
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-22 16:03:02 +1300

    Added dob and phoneNumber as required

--- introAppDev-frontEnd-Assignment/src/routes/auth/register/+page.svelte
+++ introAppDev-frontEnd-Assignment/src/routes/auth/register/+page.svelte
@@ -1,57 +1,76 @@
 <script>
-    let { form } = $props();
+  let { form } = $props();
 </script>
 
 <h1>Register</h1>
 
 <form method="POST" action="?/register">
-    <label for="firstName">First Name</label>
-    <input
+  <label for="firstName">First Name</label>
+  <input
     id="firstName"
-    name="firstName" 
+    name="firstName"
     type="text"
-    value={form?.firstName ?? ''}
+    value={form?.firstName ?? ""}
     placeholder="Enter First Name"
-    />
+  />
 
-    <label for="lastName">Last Name</label>
-    <input
+  <label for="lastName">Last Name</label>
+  <input
     id="lastName"
-    name="lastName" 
+    name="lastName"
     type="text"
-    value={form?.lastName ?? ''}
+    value={form?.lastName ?? ""}
     placeholder="Enter Last Name"
-    />
+  />
 
-    <label for="email">Email Address</label>
-    <input
+  <label for="email">Email Address</label>
+  <input
     id="email"
-    name="email" 
+    name="email"
     type="email"
-    value={form?.email ?? ''}
+    value={form?.email ?? ""}
     placeholder="Enter Email"
-    />
+  />
 
-    <label for="password">Password</label>
-    <input
+  <label for="dob">Date of Birth</label>
+  <input
+    type="date"
+    id="dob"
+    name="dob"
+    value={form?.dob ?? ""}
+    required
+  />
+
+  <label for="password">Password</label>
+  <input
     id="password"
-    name="password" 
+    name="password"
     type="password"
-    value={form?.password ?? ''}
+    value={form?.password ?? ""}
     placeholder="Enter Password"
-    />
+  />
 
-    <label for="role">Role</label>
-    <select name="role" id="role">
-        <option value="ADMIN" selected={form?.role === 'ADMIN'}>Admin</option>
-        <option value="NORMAL" selected={form?.role === 'NORMAL'}>Normal</option>
-    </select>
+  <label for="phoneNumber">Phone Number</label>
+  <input
+    type="tel"
+    id="phoneNumber"
+    name="phoneNumber"
+    value={form?.phoneNumber ?? ""}
+    placeholder="Enter Phone Number"
+  />
+
+  <label for="role">Role</label>
+  <select name="role" id="role">
+    <option value="ADMIN" selected={form?.role === "ADMIN"}>Admin</option>
+    <option value="NORMAL" selected={form?.role === "NORMAL"}>Normal</option>
+  </select>
+  <button type="submit">Submit</button>
 </form>
 
 {#if form?.success}
-	<p>{form.message}</p>
+  <p>{form.message}</p>
 {/if}
 
 {#if form?.success === false}
-	<p>{form.error}</p>
+  <p>{form.error}</p>
 {/if}
