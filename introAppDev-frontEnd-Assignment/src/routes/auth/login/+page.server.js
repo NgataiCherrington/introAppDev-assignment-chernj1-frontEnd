e679b69753d5a4a33eac68c0f118da0db99cdfcd
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-22 16:07:34 +1300

    Login action done

--- introAppDev-frontEnd-Assignment/src/routes/auth/login/+page.server.js
+++ introAppDev-frontEnd-Assignment/src/routes/auth/login/+page.server.js
@@ -3,4 +3,51 @@ import { fail, redirect } from "@sveltejs/kit" ;
 
 const API_BASE_URL = env.API_BASE_URL || "http://localhost:3000";
 
-export
+export const actions = {
+    login: async (event, cookies) => {
+        const formData = await event.formData();
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const user = { email, password };
+
+        let res, data;
+
+        try {
+            res = await fetch(`${API_BASE_URL}/api/auth/login`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+              });
+
+              data = await res.json();
+        } catch(err) {
+            return fail(500, {
+                success: false,
+                error: err.message,
+                email,
+                password,
+            });
+        }
+
+        if(!res.ok) {
+            return fail(401, {
+                success: false,
+                error: data.message,
+                email,
+                password,
+            });
+        }
+
+        cookies.set("token", data.token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 60*60,
+            path:"/",
+        });
+
+        redirect(303, "/");
+    }
+}
