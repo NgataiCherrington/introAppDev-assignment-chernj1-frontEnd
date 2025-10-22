3ec58ae1aeb92c76cf8fd9ecd87a359f8f46a1fd
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-22 17:37:55 +1300

    Showing loaded data on page

---
+++ introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let data; // contains what you returned from load()
+  </script>
+  
+  {#if data.myData}
+    <pre>{JSON.stringify(data.myData, null, 2)}</pre>
+  {:else}
+    <p>Error: {data.error}</p>
+  {/if}
