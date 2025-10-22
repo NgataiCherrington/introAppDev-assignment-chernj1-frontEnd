1caed04b4edcfa1b377f8cb01fe92a8c2ec47604
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-22 15:36:23 +1300

    Register form done

---
+++ introAppDev-frontEnd-Assignment/src/routes/auth/register/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+    let { form } = $props();
+</script>
+
+<h1>Register</h1>
+
+<form method="POST" action="?/register">
+    <label for="firstName">First Name</label>
+    <input
+    id="firstName"
+    name="firstName" 
+    type="text"
+    value={form?.firstName ?? ''}
+    placeholder="Enter First Name"
+    />
+
+    <label for="lastName">Last Name</label>
+    <input
+    id="lastName"
+    name="lastName" 
+    type="text"
+    value={form?.lastName ?? ''}
+    placeholder="Enter Last Name"
+    />
+
+    <label for="email">Email Address</label>
+    <input
+    id="email"
+    name="email" 
+    type="email"
+    value={form?.email ?? ''}
+    placeholder="Enter Email"
+    />
+
+    <label for="password">Password</label>
+    <input
+    id="password"
+    name="password" 
+    type="password"
+    value={form?.password ?? ''}
+    placeholder="Enter Password"
+    />
+
+    <label for="role">Role</label>
+    <select name="role" id="role">
+        <option value="ADMIN" selected={form?.role === 'ADMIN'}>Admin</option>
+        <option value="NORMAL" selected={form?.role === 'NORMAL'}>Normal</option>
+    </select>
+</form>
+
+{#if form?.success}
+	<p>{form.message}</p>
+{/if}
+
+{#if form?.success === false}
+	<p>{form.error}</p>
+{/if}
