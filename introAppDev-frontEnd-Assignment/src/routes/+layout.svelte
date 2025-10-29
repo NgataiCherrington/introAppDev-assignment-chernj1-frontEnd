d34370ef2b1105753f18d71c005adb60ed190848
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-29 15:08:08 +1300

    Import navabar into layout page

--- introAppDev-frontEnd-Assignment/src/routes/+layout.svelte
+++ introAppDev-frontEnd-Assignment/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+	import NavBar from '$lib/NavBar.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
+<NavBar />
 {@render children?.()}
