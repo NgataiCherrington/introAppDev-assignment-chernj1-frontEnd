333a8e805016fbc9a0a9f7feebee833fb0c55a8d
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-30 14:20:50 +1300

    Added invalidateAll function to load after successful delete

--- introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.svelte
+++ introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.svelte
@@ -1,20 +1,36 @@
 <script>
   import { enhance } from "$app/forms";
+  import { invalidateAll } from "$app/navigation";
 
   let data = $props();
 
-  let users = $state(data.users?.data?.data || []);
-  let error = $state(data.error);
+  //   let users = $state(data.users?.data?.data || []);
+  //   let error = $state(data.error);
+
+  $effect(() => {
+	console.log("Data from load:", data);
+  });
 </script>
 
 {#if error}
   <p>{error}</p>
-{:else if users.length > 0}
+{:else if data.users?.data?.data && data.users.data.data.length > 0}
   <h1>Users</h1>
   <ul>
-    {#each users as user}
+    {#each data.users.data.data as user}
       <li>{user.firstName} {user.lastName}</li>
-      <form method="POST" action="?/delete" use:enhance>
+      <form 
+	  	method="POST" 
+		action="?/delete" 
+		use:enhance={() => {
+			return async ({ result, update }) => {
+				if(result.type === 'success') {
+					await invalidateAll();
+				}
+				await update();
+			}
+		}
+		}>
         <input type="hidden" name="id" value={user.id} />
         <button type="submit">Delete</button>
       </form>
