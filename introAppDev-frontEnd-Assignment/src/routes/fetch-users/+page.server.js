74838eb80ec1c5563a7a0e0382ef46de6eb3e6d2
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-30 14:01:52 +1300

    Removed hard code of api base url

--- introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.server.js
+++ introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.server.js
@@ -1,48 +1,47 @@
 import { env } from "$env/dynamic/private";
 import { error } from "@sveltejs/kit";
 
-const PUBLIC_API_BASE_URL = "https://ngatai-introappdev-backend.onrender.com" || "http://localhost:3000";
-
+const PUBLIC_API_BASE_URL = env.PUBLIC_API_BASE_URL || "http://localhost:3000";
 
 export const load = async ({ fetch }) => {
-    try {
-        console.log("Fetching users from:", PUBLIC_API_BASE_URL);
+  try {
+    console.log("Fetching users from:", PUBLIC_API_BASE_URL);
 
-        const res = await fetch(`${PUBLIC_API_BASE_URL}/api/users`);
+    const res = await fetch(`${PUBLIC_API_BASE_URL}/api/users`);
 
-        console.log("Response status:", res.status);
-        if(!res.ok){
-            throw new Error("Failed to fetch data");
-        }
-        const users = await res.json();
-
-        return {
-            users,
-            error: null,
-        };
-    } catch (err) {
-        return {
-            users: [],
-            error: err.message,
-        };
+    console.log("Response status:", res.status);
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
     }
+    const users = await res.json();
+
+    return {
+      users,
+      error: null,
+    };
+  } catch (err) {
+    return {
+      users: [],
+      error: err.message,
+    };
+  }
 };
 
 export const actions = {
-    delete: async ({ request }) => {
-        const formData = await request.formData();
-        const id = formData.get("id");
+  delete: async ({ request }) => {
+    const formData = await request.formData();
+    const id = formData.get("id");
 
-        try {
-            const res = await fetch(`${PUBLIC_API_BASE_URL}/api/users/${id}`, {
-                method: "DELETE",
-            });
+    try {
+      const res = await fetch(`${PUBLIC_API_BASE_URL}/api/users/${id}`, {
+        method: "DELETE",
+      });
 
-            const data = await res.json();
+      const data = await res.json();
 
-            return { success: true, message: data.message };
-        } catch(err) {
-            return { success: false, error: err.message };
-        }
-    },
+      return { success: true, message: data.message };
+    } catch (err) {
+      return { success: false, error: err.message };
+    }
+  },
 };
