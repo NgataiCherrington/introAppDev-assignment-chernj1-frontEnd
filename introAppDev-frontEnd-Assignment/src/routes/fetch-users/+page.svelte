40321090672b6022f0b2385354cf80caf5bcbdd1
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-30 14:02:04 +1300

    Wrapped user view in a form

--- introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.svelte
+++ introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.svelte
@@ -1,37 +1,38 @@
 <script>
-	import { env } from "$env/dynamic/public";
-	import { onMount } from "svelte";
+  import { env } from "$env/dynamic/public";
+  import { onMount } from "svelte";
 
-	let users = $state([]);
-	let error = $state(null);
+  let users = $state([]);
+  let error = $state(null);
 
-	const PUBLIC_API_BASE_URL = "https://ngatai-introappdev-backend.onrender.com" || "http://localhost:3000";
+  const PUBLIC_API_BASE_URL =
+    env.PUBLIC_API_BASE_URL || "http://localhost:3000";
 
-	onMount(async () => {
-		try { 
-			const res = await fetch(`${PUBLIC_API_BASE_URL}/api/users`);
-			const json = await res.json();
+  onMount(async () => {
+    try {
+      const res = await fetch(`${PUBLIC_API_BASE_URL}/api/users`);
+      const json = await res.json();
 
-			users = json.data.data // Access the nested data.data array
-		} catch(err) {
-			error = err.message;
-		}
-	});
+      users = json.data.data; // Access the nested data.data array
+    } catch (err) {
+      error = err.message;
+    }
+  });
 </script>
 
-{#if error}
-	<p>{error}</p>
-{:else if users.length > 0}
-	<h1>Users</h1>
-	<ul>
-		{#each users as user}
-			<li>{user.firstName} {user.lastName}</li>
-			<form method="POST" action="?/delete">
-				<input type="hidden" name="id" value={user.id} />
-				<button type="submit">Delete</button>
-			</form>
-		{/each}
-	</ul>
-{:else}
-	<p>No users found</p>
-{/if}
+<form method="POST" action="?/delete">
+  {#if error}
+    <p>{error}</p>
+  {:else if users.length > 0}
+    <h1>Users</h1>
+    <ul>
+      {#each users as user}
+        <li>{user.firstName} {user.lastName}</li>
+        <input type="hidden" name="id" value={user.id} />
+        <button type="submit">Delete</button>
+      {/each}
+    </ul>
+  {:else}
+    <p>No users found</p>
+  {/if}
+</form>
