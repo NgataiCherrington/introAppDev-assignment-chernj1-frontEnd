3669447fcbe52f26b802e5cdd63cd6071413744f
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-30 14:12:53 +1300

    Removed onMount and passing data in as props

--- introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.svelte
+++ introAppDev-frontEnd-Assignment/src/routes/fetch-users/+page.svelte
@@ -1,24 +1,10 @@
 <script>
   import { enhance } from "$app/forms";
-  import { env } from "$env/dynamic/public";
-  import { onMount } from "svelte";
 
-  let users = $state([]);
-  let error = $state(null);
+  let data = $props();
 
-  const PUBLIC_API_BASE_URL =
-    env.PUBLIC_API_BASE_URL || "http://localhost:3000";
-
-  onMount(async () => {
-    try {
-      const res = await fetch(`${PUBLIC_API_BASE_URL}/api/users`);
-      const json = await res.json();
-
-      users = json.data.data; // Access the nested data.data array
-    } catch (err) {
-      error = err.message;
-    }
-  });
+  let users = $state(data.users?.data?.data || []);
+  let error = $state(data.error);
 </script>
 
 {#if error}
