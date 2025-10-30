9a7b46e1d3e1c1596ef38fdd6a32b71b3ae1607c
NgataiCherrington <chernj1@student.op.ac.nz>
2025-10-30 14:37:14 +1300

    Changed name of directory

---
+++ introAppDev-frontEnd-Assignment/src/routes/user-display/+page.server.js
@@ -0,0 +1,47 @@
+import { env } from "$env/dynamic/private";
+import { error } from "@sveltejs/kit";
+
+const API_BASE_URL = "https://ngatai-introappdev-backend.onrender.com" || "http://localhost:3000";
+
+export const load = async ({ fetch }) => {
+  try {
+    console.log("Fetching users from:", API_BASE_URL);
+
+    const res = await fetch(`${API_BASE_URL}/api/users`);
+
+    console.log("Response status:", res.status);
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const users = await res.json();
+
+    return {
+      users,
+      error: null,
+    };
+  } catch (err) {
+    return {
+      users: { data: { data: []} },
+      error: err.message,
+    };
+  }
+};
+
+export const actions = {
+  delete: async ({ request, fetch }) => {
+    const formData = await request.formData();
+    const id = formData.get("id");
+
+    try {
+      const res = await fetch(`${API_BASE_URL}/api/users/${id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      return { success: true, message: data.message };
+    } catch (err) {
+      return { success: false, error: err.message };
+    }
+  },
+};
